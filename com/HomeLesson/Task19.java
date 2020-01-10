6883cc878e27fa5f78a7956fb38411527412e57f
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-01-10 20:00:37 +0300

    Task 19

---
+++ com/HomeLesson/Task19.java
@@ -0,0 +1,19 @@
+package com.HomeLesson;
+
+import java.util.Scanner;
+
+public class Task19 {
+    public static void main(String[] args) {
+        String str = "Test string, with several, characters and commas.";
+        int count = 0;
+        count++;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ' ')
+                count ++;
+
+        }
+
+        System.out.println("Вы ввели "+count+" слов");
+    }
+}
+
