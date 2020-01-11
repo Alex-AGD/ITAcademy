069c985d1cb2a9e57feaaf9bafdf9097eb1cda15
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-01-11 22:16:03 +0300

    Correction

--- com/HomeLesson/Task19.java
+++ com/HomeLesson/Task19.java
@@ -2,16 +2,19 @@ package com.HomeLesson;
 
 public class Task19 {
     public static void main(String[] args) {
-        String str = "Test string, with several, characters and commas.";
+        String str = "Test string, with several,  characters and commas.";
+        str = str.replaceAll("  ", " ");
         int count = 0;
         count++;
         for (int i = 0; i < str.length(); i++) {
+
             if(str.charAt(i) == ' ')
                 count ++;
 
         }
-
         System.out.println("Вы ввели "+count+" слов");
     }
+
+
 }
 
