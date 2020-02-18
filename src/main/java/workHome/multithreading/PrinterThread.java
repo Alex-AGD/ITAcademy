01ad07d23ddc11094a7e8269c1c8a9b386b22f79
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-02-19 00:47:24 +0300

    multithreading (Thread,Runable)

--- src/main/java/workHome/multithreading/PrinterThread.java
+++ src/main/java/workHome/multithreading/PrinterThread.java
@@ -1,7 +1,11 @@
 package main.java.workHome.multithreading;
 
 public class PrinterThread extends Thread {
+private String name;
 
+    public PrinterThread(String name) {
+        this.name = name;
+    }
 
     @Override
     public void run() {
@@ -11,10 +15,14 @@ public class PrinterThread extends Thread {
 
     private void someMethod() {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Theard name " + Thread.currentThread().getName() + " i = " + i);
+            System.out.printf("I'm %s\n", name  + Thread.currentThread().getName() + " i = " + i);
 
 
         }
+    }
+
+    public void getPriority(int i) {
+        System.out.printf("I'm %s\n", name  + Thread.currentThread().getName() + " Приоритет получен!!!");
     }
 }
 
--- src/main/java/workHome/multithreading/Runner.java
+++ src/main/java/workHome/multithreading/Runner.java
@@ -2,36 +2,43 @@ package main.java.workHome.multithreading;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
-        PrinterThread first = new PrinterThread();
+        PrinterThread first = new PrinterThread("Thread first ");
+        Thread.yield();
         first.start();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         //first.run();
 
-        PrinterThread second = new PrinterThread();
+        PrinterThread second = new PrinterThread("Thread second ");
+        second.setPriority(Thread.MAX_PRIORITY);
         second.start();
         Thread.sleep(2000);
         //second.run();
 
 
-        Runable myRunable = new Runable();
+        Runable myRunable = new Runable("Thread myRunable ");
         Thread thread = new Thread(myRunable);
+        thread.setPriority(Thread.MAX_PRIORITY);
         thread.start();
-        Thread.sleep(3000);
+        thread.join();
+        Thread.sleep(2000);
 
-        Runable myRunableTwo = new Runable();
+        Runable myRunableTwo = new Runable("Thread myRunableTwo ");
         Thread threadTwo = new Thread(myRunableTwo);
+
         threadTwo.start();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
 
-        new Runable().run();
+        new Runable("Thread Runable static ").run();
         Thread.sleep(3000);// выполняет метод run последовательно без использования Thread
 
 
-        new Runable.MyThread().start();
-        Thread.sleep(3000);
-        new Runable.MyThread().start();
-        Thread.sleep(3000);
+        new Runable.MyThread("Thread static MyThreadOne ").start();
+        Thread.sleep(2000);
+
+        new Runable.MyThread("Thread static MyThreadTwo ").start();
+        Thread.yield(); //дает вероятность что поток  выполнится раньше
+        Thread.sleep(2000);
 
 
     }
@@ -39,23 +46,41 @@ public class Runner {
 }
 
 class Runable implements Runnable {
+    private String name;
+
+    public Runable(String name) {
+        this.name = name;
+    }
+
     @Override
     public void run() {
         for (int i = 0; i < 3; i++) {
-            System.out.println("Thread name " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("I'm %s\n", name  + Thread.currentThread().getName());
         }
 
     }
 
     static class MyThread extends Thread {
+        private String name;
+
+        public MyThread(String name) {
+            this.name = name;
+        }
+
         @Override
         public void run() {
             for (int i = 0; i < 5; i++) {
-                System.out.println("Thread name " + Thread.currentThread().getName() + " i = " + i);
+
+                System.out.printf("I'm %s\n", name  + Thread.currentThread().getName() + " i = " + i);
             }
-        }
-    }
-}
+        }}}
+
+
 
 
 
