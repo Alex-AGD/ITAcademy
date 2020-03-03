d9c6812f1b64095c66b9a9136346f26369e83cb7
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-03-03 21:53:52 +0300

    Hibernate

--- src/main/java/workHome/test.java
+++ src/main/java/workHome/test.java
@@ -1,9 +1,11 @@
 package workHome;
 
+import workHome.stream.soundStream.Sound;
+
 public class test {
     public static void main(String[] zaza) {
 
-
+        Sound.playSound("src/main/resources/campuchaMan.wav").join();
 
 
 
